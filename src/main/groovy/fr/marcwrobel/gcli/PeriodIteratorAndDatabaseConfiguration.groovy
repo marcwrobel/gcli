4f0f68220f6848d4626995c2ac9c6d16788da9b5
Marc Wrobel <marc.wrobel@gmail.com>
2020-02-26 22:14:40 +0100

    Add support for delay in PeriodIteratorAndDatabaseConfiguration (fixes #5)

--- src/main/groovy/fr/marcwrobel/gcli/PeriodIteratorAndDatabaseConfiguration.groovy
+++ src/main/groovy/fr/marcwrobel/gcli/PeriodIteratorAndDatabaseConfiguration.groovy
@@ -1,6 +1,9 @@
 package fr.marcwrobel.gcli
 
+import java.time.Duration
 import java.time.LocalDateTime
+
+import static java.time.LocalDateTime.now
 
 /**
  * Basic configuration for a groovy script with database capabilities and period iterator
@@ -15,17 +18,38 @@ class PeriodIteratorAndDatabaseConfiguration extends DatabaseConfiguration {
     cli.f longOpt: 'from', args: 1, "start timestamp, formatted as YYYY-MM-DDTHH:mm:ss (default = now - duration)."
     cli.t longOpt: 'to', args: 1, "end timestamp, formatted as YYYY-MM-DDTHH:mm:ss (default = now)."
     cli.s longOpt: 'step', args: 1, "Step's duration, specified as DAILY, HOURLY, MINUTELY (default = HOURLY)."
+    cli.d longOpt: 'delay', args: 1, "Delay, specified as an ISO-8601 duration (default = none)."
   }
 
   LocalDateTime from() {
-    step().adjust(options.f ? LocalDateTime.parse(options.f as String) : step().previous(LocalDateTime.now()))
+    def from = options.f ? LocalDateTime.parse(options.f as String) : step().previous(now())
+
+    from = step().adjust(from)
+    def delay = delay()
+    if(delay) {
+      from = from - delay
+    }
+
+    return from
   }
 
   LocalDateTime to() {
-    step().adjust(options.t ? LocalDateTime.parse(options.t as String) : LocalDateTime.now())
+    def to = options.t ? LocalDateTime.parse(options.t as String) : now()
+
+    to = step().adjust(to)
+    def delay = delay()
+    if(delay) {
+      to = to - delay
+    }
+
+    return to
   }
 
   Step step() {
     options.s ? Step.valueOf((options.s as String).toUpperCase()) : Step.HOURLY
+  }
+
+  Duration delay() {
+    options.d ? Duration.parse(options.d as String) : null
   }
 }
