b05650af069757f30ee91687c26b5096103c6cb6
Marc Wrobel <marc.wrobel@gmail.com>
2020-02-26 22:08:13 +0100

    Add support for databases other than PostgreSQL (fixes #6)

--- src/main/groovy/fr/marcwrobel/gcli/DatabaseConfiguration.groovy
+++ src/main/groovy/fr/marcwrobel/gcli/DatabaseConfiguration.groovy
@@ -7,22 +7,24 @@ import groovy.sql.Sql
  */
 class DatabaseConfiguration extends LoggingConfiguration {
 
+  private static final String POSTGRES_DRIVER = 'org.postgresql.Driver'
+
+  private final File passFile = new File(userHome, '.dbpass')
   private final File pgPassFile = new File(userHome, '.pgpass')
 
   DatabaseConfiguration(CliTexts texts) {
     super(texts)
-    cli.db longOpt: 'db-name', args: 1, 'Database name (default : the current user name)'
+    cli.dc longOpt: 'db-connection', args: 1, 'Database connection string (default : a postgresql connection URL based on db-name)'
     cli.du longOpt: 'db-user', args: 1, 'Database user name (default : the current user name)'
+    cli.dd longOpt: 'db-driver', args: 1, 'Database driver (default : org.postgresql.Driver)'
+    cli.db longOpt: 'db-name', args: 1, 'Database name (default : the current user name)'
   }
 
   def withSql(Closure<Integer> c) {
-    def dbName = databaseName
     def dbUser = databaseUser
-    def dbHost = retrieveValueFromPgPass(dbUser, 0, 'localhost')
-    def dbPort = retrieveValueFromPgPass(dbUser, 1, '5432')
-    def dbUrl = "jdbc:postgresql://${dbHost}:${dbPort}/${dbName}"
-    def dbPassword = retrieveValueFromPgPass(dbUser, 4, dbUser)
-    def dbDriver = 'org.postgresql.Driver'
+    def dbUrl = databaseUrl
+    def dbPassword = databasePassword
+    def dbDriver = databaseDriver
 
     Integer returnValue = 0
     Sql.withInstance(dbUrl, dbUser, dbPassword, dbDriver) { Sql sql ->
@@ -51,6 +53,42 @@ class DatabaseConfiguration extends LoggingConfiguration {
   }
 
   final String getDatabaseUser() {
-    return options.du ?: databaseName
+    return options.du ?: user
+  }
+
+  final String getDatabaseUrl() {
+    String dbUrl = options.dc
+
+    if (!dbUrl) {
+      def dbName = databaseName
+      def dbUser = databaseUser
+      def dbHost = retrieveValueFromPgPass(dbUser, 0, 'localhost')
+      def dbPort = retrieveValueFromPgPass(dbUser, 1, '5432')
+      dbUrl = "jdbc:postgresql://${dbHost}:${dbPort}/${dbName}"
+    }
+
+    return dbUrl
+  }
+
+  final String getDatabasePassword() {
+    if (databaseDriver == POSTGRES_DRIVER) {
+      def dbUser = databaseUser
+      return retrieveValueFromPgPass(dbUser, 4, dbUser)
+    }
+
+    if(!passFile.exists()) {
+      throw new IllegalStateException(passFile + ' must exist !')
+    }
+
+    def passFileLines = passFile.readLines()
+    if(passFileLines.size() == 0) {
+      throw new IllegalStateException(passFile + ' must not be empty !')
+    }
+
+    return passFileLines.get(0)
+  }
+
+  final String getDatabaseDriver() {
+    return options.dd ?: POSTGRES_DRIVER
   }
 }
